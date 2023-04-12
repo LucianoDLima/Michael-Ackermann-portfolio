b8ab3d4751f5d069c86993e71ccaed81621ffe8c
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-04-11 22:40:25 -0300

    var name

--- src/data/ScreenSize.tsx
+++ src/data/ScreenSize.tsx
@@ -3,8 +3,8 @@ const bodyFontSize:number = +window
   .getPropertyValue('font-size')
   .slice(0, -2);
 
-const emToPx = (em:number) => em * bodyFontSize
+const pxToEm = (em:number) => em * bodyFontSize
 
-export const screenSize:number = emToPx(76.25)
+export const screenSize:number = pxToEm(76.25)
 
 // For responsiveness, this ensures the screen size will respect the users current font size in case they change their browser's.
